3c33511e075deba314bc2b388493337744752c39
Stanislav Novitski <stani2980@hotmail.com>
2018-10-07 22:12:09 +0200

    closer

--- plotters.py
+++ plotters.py
@@ -9,13 +9,12 @@ def count_on_release_date(movies_df):
     pass
     
 def runtime_and_release(movies_df):
-    release_date = pd.to_datetime(movies_df['release_date'], yearfirst=True , errors='coerce', unit='s')
-    print(release_date)
+    release_date = pd.to_datetime(movies_df['release_date'], yearfirst=True , errors='coerce')
 
     runtime = pd.to_numeric(movies_df['runtime'], errors='raise', downcast='unsigned')  ### not needed....
     
 
     df = pd.DataFrame(release_date,runtime)
+    # print(df['release_date'][:1],type(df['release_date'][:1]))
 
-    df.plot.scatter(x='release_date',y='runtime')
-    df.plot.show()
+    # df.plot.scatter(x='release_date',y='runtime')
