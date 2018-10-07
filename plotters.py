7c8515fa227aa866f7d0949da4883fc751bf60e4
Stanislav Novitski <stani2980@hotmail.com>
2018-10-07 23:01:57 +0200

    done with mikkels great help

--- plotters.py
+++ plotters.py
@@ -4,14 +4,14 @@ import datetime
 import numpy as np
 
 
-
-# THIS IS A HOUDINI FIX 
+# THIS IS A HOUDINI FIX
 def runtime_and_release(movies_df):
-    release_date = pd.to_datetime(movies_df['release_date'], errors='')
+    movies_df = movies_df[movies_df['release_date'].str.len() == 10]
+
+    release_date = pd.to_datetime(movies_df['release_date'], errors='coerce')
     runtime = pd.to_numeric(movies_df['runtime'], errors='raise', downcast='unsigned')
 
     df = pd.DataFrame(release_date, runtime).reset_index()
-    df = df[df['release_date'].notnull()]
 
     df.plot(x='release_date', y='runtime', style='.')
     plt.show()
